4834503fd565bb1be2d77c8f3ea16467f1bbcd49
Mingbo <mingbo@mingbo-ThinkPad-T60.(none)>
2014-04-26 23:19:53 -0700

    working on login

---
+++ included.php
@@ -0,0 +1,8 @@
+<?php
+	session_start();
+	$hostname = 'springlight.db';
+	$username = 'andrewgu12';
+	$password = '0d06et1';
+	$dbname = 'lightBack';
+	$conn = mysqli_connect($hostname, $username, $password, $dbname);
+?>
---
+++ menu.php
@@ -0,0 +1,7 @@
+<nav id="nav-bar" class="top-bar" data-topbar>
+              <ul class="left">
+                <li><a href="#">home</a></li>
+                <li><a href="#">SAT I</a></li>
+                <li><a href="#">SAT II</a></li>
+              </ul>
+            </nav>
