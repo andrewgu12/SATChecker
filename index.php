0ddf5554bf69fce79ab854cb93f84b23d97b6989
Mingbo <mingbo@mingbo-ThinkPad-T60.(none)>
2014-04-28 10:12:36 -0700

    login done

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php //require_once("included.php"); ?>
+<?php 
+  require_once("included.php"); 
+  if(isset($_REQUEST['studID']))
+  {
+      $user = $_REQUEST['studID'];
+      $pass = sha1($_REQUEST['pass']);
+      $results = mysqli_query($conn, "SELECT * FROM `Students` WHERE `ID`='$user' LIMIT 1");
+      $student = mysqli_fetch_array($results);
+      $_SESSION['user'] = $user;
+      header("Location: index_home.php");
+  }
+  if(isset($_SESSION['user'])) 
+  {
+      header("Location: index_home.php");
+      die;
+  }
+?>
 <!doctype html>
 <html>
   <head>
@@ -28,7 +44,7 @@
                 <label>Username</label>
               </div>
               <div class="large-9 columns">
-                <input type="text" />
+                <input type="text" id="studID" name="studID" />
               </div>
             </div>
            <div class="row">
@@ -36,7 +52,7 @@
                 <label>Password</label>
               </div>
               <div class="large-9 columns">
-                <input type="password" />
+                <input type="password" id="pass" name="pass"/>
               </div>
             </div>             
             <a href="#" class="button right" id="loginButton">Login</a>
