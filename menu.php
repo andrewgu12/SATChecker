f983dcaee66ef3c039be56ee92284efe77d81119
Mingbo <mingbo@mingbo-ThinkPad-T60.(none)>
2014-05-10 18:38:07 -0700

    working on menu

--- menu.php
+++ menu.php
@@ -1,7 +1,12 @@
 <nav id="nav-bar" class="top-bar" data-topbar>
               <ul class="left">
-                <li><a href="#">home</a></li>
-                <li><a href="#">SAT I</a></li>
+                <li class="nodrop"><a href="#">home</a></li>
+                <li class="has-dropdown"><a href="#">SAT I</a>
+                	<ul class="dropdown">
+                		<li class="drop"><a href="#">Score a Test</a></li>
+                		<li class="drop"><a href="#">Past Tests</a></li>
+                	</ul>
+                </li>
                 <li><a href="#">SAT II</a></li>
               </ul>
             </nav>
