051adce161bc299ccc72c2e36abeb0a1308cd108
andrewgu12 <andrew@mingbogu.com>
2014-05-15 00:01:36 -0700

    Update look of scoreTest.php

--- scoreTest.php
+++ scoreTest.php
@@ -28,10 +28,29 @@
             <?php
               //$counter = 0;              
               echo "<form id='scoreI'>\n";
+              echo "<div class='row'>";
+              echo "<div class='large-4 small-4 columns'>";
+              echo "<label>Month";
+              echo "<select>";
+              echo "<option value=''></option></select></label></div>";
+              echo "<div class='large-4 small-4 columns'>";
+              echo "<label>Day";
+              echo "<select>";
+              echo "<option value=''></option></select></label></div>";
+              echo "<div class='large-4 small-4 columns'>";
+              echo "<label>Year";
+              echo "<select>";
+              echo "<option value=''></option></select></label></div>";
+              /*echo "<div class='row'><div class='large-3 small-3 columns'>";
+              echo "<label for='essay' class='left'>Essay Score: </label>";
+              echo "</div>";
+              echo "<div class='small-9 large-9 columns'>";
+              echo "<input type='text' id='essay' placeholder='Enter Score here'>";
+              echo "</div></div>";*/
               for($outerCounter = 1; $outerCounter <= 10; $outerCounter++) {
                 echo "<h3>Section $outerCounter</h3>";
                 echo "<div class='row'>\n";
-                for($counter = 1; $counter <= 40; $counter++) 
+                for($counter = 1; $counter <= 48; $counter++) 
                 {
                   $boxNumber = 'box' . $counter;
                   echo "<div class='large-1 small-1 columns testBox'>\n";
@@ -48,8 +67,10 @@
                 echo "</div>\n";
               }
               echo "<h3>Student Produced Response: </h3>";
+              echo "<label>Part of section: ";
+              echo "<select><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='5'>5</option><option value='6'>6</option><option value='7'>7</option><option value='8'>8</option><option value='9'>9</option><option value='10'>10</option></select>";
               echo "<div class='row'>";
-              for($counter = 1; $counter <= 10; $counter++)
+              for($counter = 1; $counter <= 12; $counter++)
               {
                 echo "<div class='large-2 small-2 columns testBox'>";
                 $counterNumber = $counter + 8;
@@ -63,6 +84,13 @@
                 }
               }
               echo "</div>";
+              echo "<div class='row'>";
+              echo "<div class='large-3 small-3 columns'>";
+              echo "<label for='essay' class='left'>Essay Score: </label></div>";
+              echo "<div class='large-9 small-9 columns'>";            
+              echo "<input type='text' id='essay' placeholder='Enter Score here'>";
+              echo "</div></div>";
+              echo "<input type='submit' class='button right' id='loginButton' value='Calculate!'>";
               echo "</form>\n";
             ?>
         
